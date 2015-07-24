be04cb357f0f7d9ab6c659619e0960c5157b70e8
Thiago Senna <trsenna@gmail.com>
2015-07-24 02:44:19 -0300

    added bower initial support

--- www/app/main.js
+++ www/app/main.js
@@ -1,39 +1,40 @@
-﻿requirejs.config({
+﻿requirejs.config( {
     paths: {
-        'text': '../lib/require/text',
-        'durandal':'../lib/durandal/js',
-        'plugins' : '../lib/durandal/js/plugins',
-        'transitions' : '../lib/durandal/js/transitions',
-        'knockout': '../lib/knockout/knockout-3.1.0',
-        'bootstrap': '../lib/bootstrap/js/bootstrap',
-        'jquery': '../lib/jquery/jquery-1.9.1'
+        'text': 'bower_components/requirejs-text/text',
+        'durandal': 'bower_components/durandal/js',
+        'plugins': 'bower_components/durandal/js/plugins',
+        'transitions': 'bower_components/durandal/js/transitions',
+        'knockout': 'bower_components/knockout/dist/knockout',
+        'bootstrap': 'bower_components/bootstrap/js/bootstrap',
+        'jquery': 'bower_components/jquery/dist/jquery'
     },
     shim: {
         'bootstrap': {
-            deps: ['jquery'],
+            deps: [ 'jquery' ],
             exports: 'jQuery'
-       }
+        }
     }
-});
+} );
 
-define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (system, app, viewLocator) {
+define( [ 'durandal/system', 'durandal/app', 'durandal/viewLocator' ], function ( system, app, viewLocator ) {
+    
     //>>excludeStart("build", true);
-    system.debug(true);
+    system.debug( true );
     //>>excludeEnd("build");
 
     app.title = 'Durandal Starter Kit';
 
-    app.configurePlugins({
-        router:true,
+    app.configurePlugins( {
+        router: true,
         dialog: true
-    });
+    } );
 
-    app.start().then(function() {
+    app.start().then( function () {
         //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
         //Look for partial views in a 'views' folder in the root.
         viewLocator.useConvention();
 
         //Show the app by setting the root view model for our application with a transition.
-        app.setRoot('viewmodels/shell', 'entrance');
-    });
-});
+        app.setRoot( 'viewmodels/shell', 'entrance' );
+    } );
+} );
