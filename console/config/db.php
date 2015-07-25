36b261400ae083a6b6e7984ced317b33331bec53
Thiago Senna <trsenna@gmail.com>
2015-07-25 12:18:31 -0300

    the new api/console structure

---
+++ console/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=mazoo',
+    'username' => 'mazoo',
+    'password' => 'mazoo@secret',
+    'charset' => 'utf8',
+];
