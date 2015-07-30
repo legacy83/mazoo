c5fa00ff264709bd3d0961eff289fe1854e52658
Thiago Senna <trsenna@gmail.com>
2015-07-29 21:21:11 -0300

    saving progress

--- www/app/viewmodels/shell.js
+++ www/app/viewmodels/shell.js
@@ -1,17 +1,16 @@
 ﻿define(['plugins/router', 'durandal/app'], function (router, app) {
     return {
         router: router,
-        search: function() {
+        search: function () {
             //It's really easy to show a message box.
             //You can add custom options too. Also, it returns a promise for the user's response.
             app.showMessage('Search not yet implemented...');
         },
         activate: function () {
             router.map([
-                { route: '', title:'Welcome', moduleId: 'viewmodels/welcome', nav: true },
-                { route: 'flickr', moduleId: 'viewmodels/flickr', nav: true }
-            ]).buildNavigationModel();
-            
+                {route: '', moduleId: 'viewmodels/site/home'}
+            ]).buildNavigationModel(100);
+
             return router.activate();
         }
     };
