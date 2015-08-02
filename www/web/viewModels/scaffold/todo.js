42de46155ef0e11262e6fdb4b68d49dd95a654a4
Thiago Senna <trsenna@gmail.com>
2015-08-01 22:13:18 -0300

    enhancements done

--- www/web/viewModels/scaffold/todo.js
+++ www/web/viewModels/scaffold/todo.js
@@ -6,23 +6,18 @@ define( function ( require ) {
         ko = require( 'knockout' ),
         todoAPI = require( 'components/api/scaffold/todo' );
 
-    var viewModel = function () {
+    return function () {
         var self = this;
         self.models = ko.observableArray( [] );
-    };
 
-    viewModel.prototype.activate = function () {
-
-        var self = this;
-
-        todoAPI.all().then( function ( data ) {
-            _.forEach( data, function ( it ) {
-                self.models.push( it );
+        self.activate = function () {
+            todoAPI.all().then( function ( data ) {
+                _.forEach( data, function ( it ) {
+                    self.models.push( it );
+                } );
             } );
-        } );
+        };
 
     };
-
-    return viewModel;
 
 } );
