11f8fb8bdeb9269b0721a10b3140044009c010b0
Thiago Senna <trsenna@gmail.com>
2015-08-01 22:34:00 -0300

    enhancements done

--- www/web/components/apiUtils.js
+++ www/web/components/apiUtils.js
@@ -5,25 +5,26 @@ define( function ( require ) {
     var $ = require( 'jquery' ),
         _ = require( 'lodash' );
 
-    var apiUtils = {},
-        router = require( 'plugins/router' ),
+    var router = require( 'plugins/router' ),
         isIdentity = function ( identity ) {
             return !_.isNaN( parseInt( identity ) );
         };
 
-    apiUtils.protocol = window.location.protocol;
-    apiUtils.host = window.location.host.replace( 'www', 'api' );
+    var apiUtils = {
+        protocol: window.location.protocol,
+        host: window.location.host.replace( 'www', 'api' ),
 
-    apiUtils.routeTo = function ( r, identity ) {
-        var query = { r: r };
-        if ( isIdentity( identity ) ) {
-            query.id = parseInt( identity );
+        routeTo: function ( r, identity ) {
+            var query = { r: r };
+            if ( isIdentity( identity ) ) {
+                query.id = parseInt( identity );
+            }
+
+            var apiLocation = [ apiUtils.protocol, '//', apiUtils.host, '?' ];
+            apiLocation = apiLocation.join( '' );
+
+            return apiLocation + $.param( query );
         }
-
-        var apiLocation = [ apiUtils.protocol, '//', apiUtils.host, '?' ];
-        apiLocation = apiLocation.join( '' );
-
-        return apiLocation + $.param( query );
     };
 
     return apiUtils;
--- www/web/viewModels/site/home.js
+++ www/web/viewModels/site/home.js
@@ -1,9 +1,16 @@
 ﻿'use strict';
 
-define(function () {
+define( function ( require ) {
+
+    var system = require( 'durandal/system' );
 
     return function () {
+        var self = this;
+
+        self.activate = function () {
+            console.log( system.version );
+        };
 
     };
 
-});
+} );
