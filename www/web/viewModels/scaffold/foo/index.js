e420de8157c865f9239e6457757b32917ce2ae1f
Thiago Senna <trsenna@gmail.com>
2015-08-02 09:18:51 -0300

    added foo/index

---
+++ www/web/viewModels/scaffold/foo/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+define( function ( require ) {
+
+    var _ = require( 'lodash' ),
+        ko = require( 'knockout' ),
+        fooAPI = require( 'components/api/foo' );
+
+    return function () {
+        var self = this;
+        self.models = ko.observableArray( [] );
+
+        /**
+         * Run the custom activation
+         *
+         */
+        self.activate = function () {
+            fooAPI.all().success( function ( result ) {
+                _.forEach( result, function ( it ) {
+                    self.models.push( it );
+                } );
+            } );
+        };
+
+    };
+
+} );
