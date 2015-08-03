b65de650d8590961dfb83cf9ea4c5f645533ed8f
Thiago Senna <trsenna@gmail.com>
2015-08-03 07:56:13 -0300

    saving progress /foo/destroy/edit

---
+++ www/web/viewModels/scaffold/foo/destroy.js
@@ -0,0 +1,42 @@
+'use strict';
+
+define( function ( require ) {
+
+    var ko = require( 'knockout' ),
+        router = require( 'plugins/router' ),
+        flash = require( 'components/flashMessages' ),
+        fooAPI = require( 'components/api/scaffold/foo' );
+
+    return function () {
+        var self = this;
+        self.model = ko.observable();
+
+        /**
+         * Run the custom activation.
+         */
+        self.activate = function ( identity ) {
+            fooAPI.get( identity ).success( function ( result ) {
+                self.model( result );
+            } );
+        };
+
+        /**
+         * Destroy the model
+         */
+        self.doDestroy = function () {
+            fooAPI.destroy( self.model ).success( function () {
+                flash.success( '@foo destroyed successfully' );
+                router.navigate( '/@foo' );
+            } );
+        };
+
+        /**
+         * Cancel the model creation.
+         */
+        self.doCancel = function () {
+            router.navigate( '/@foo' );
+        };
+
+    };
+
+} );
---
+++ www/web/viewModels/scaffold/foo/edit.js
@@ -0,0 +1,50 @@
+'use strict';
+
+define( function ( require ) {
+
+    var ko = require( 'knockout' ),
+        router = require( 'plugins/router' ),
+        fooAPI = require( 'components/api/scaffold/foo' ),
+        flash = require( 'components/flashMessages' );
+
+    var Model = function ( data ) {
+        var self = this;
+        self.id = data.id;
+        self.one = ko.observable( data.one );
+        self.two = ko.observable( data.two );
+        self.three = ko.observable( data.three );
+    };
+
+    return function () {
+        var self = this;
+        self.model = ko.observable();
+
+        /**
+         * Run the custom activation.
+         */
+        self.activate = function ( identity ) {
+            fooAPI.get( identity ).success( function ( result ) {
+                self.model( new Model( result ) );
+            } );
+        };
+
+        /**
+         * Update the model
+         */
+        self.doUpdate = function () {
+            fooAPI.put( self.model ).success( function () {
+                flash.success( '@foo modified successfully' );
+                router.navigate( '/@foo' );
+            } );
+        };
+
+        /**
+         * Cancel the model creation.
+         */
+        self.doCancel = function () {
+            router.navigate( '/@foo' );
+        };
+
+    };
+
+} );
